47b185bb866c082f0640ae5595663dbc6b903887
Taylor Hogan <taylorb.hogan@gmail.com>
2022-08-11 13:35:40 -0700

    Format for isort

--- wish/settings.py
+++ wish/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 
 import dj_database_url
+from dotenv import load_dotenv
 
 load_dotenv()
 
