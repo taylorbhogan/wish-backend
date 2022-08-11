3676688ca1fe7f9b9dfa5eedd9b19b1edf2143f3
Taylor Hogan <taylorb.hogan@gmail.com>
2022-08-11 14:20:35 -0700

    Clear comments

--- wish/settings.py
+++ wish/settings.py
@@ -81,8 +81,6 @@ WSGI_APPLICATION = "wish.wsgi.application"
 
 DATABASES = {
     "default": {
-        # "ENGINE": os.environ.get("ENGINE"),
-        # "NAME": BASE_DIR / "db.sqlite3",
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("NAME"),
         "USER": os.environ.get("USER"),
