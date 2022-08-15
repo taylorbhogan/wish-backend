6e0c55f5a2b30ad2c9ba9565b4469d7fab81cdb1
Taylor Hogan <taylorb.hogan@gmail.com>
2022-08-15 13:29:30 -0700

    Create wishlist view

--- wish_app/views.py
+++ wish_app/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 
+from .models import User, Group, Gift
 
 def index(request):
     return HttpResponse("Hello, world.")
+
+def find_wishlists(request):
+    userGroups = User.objects.get(id=1).groups.all()
+    users = User.objects.filter(groups__in=userGroups)
+    if users:
+        return HttpResponse(users)
+    else:
+        return HttpResponse("No users found yet")
